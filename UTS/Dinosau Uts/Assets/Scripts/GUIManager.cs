33cb062d9b1c80878d831f8eb317391f67065e54
ArifMunawar <90273842+ArifMunawar@users.noreply.github.com>
2021-11-15 21:27:27 +0700

    UTS

---
+++ UTS/Dinosau Uts/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+
+    public void OnPlay()
+    {
+        SceneManager.LoadScene ("Game");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
